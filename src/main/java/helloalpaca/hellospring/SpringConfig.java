6072ecca8e31a1369649ec4320b55d13df2031ad
mskwon <jms393497@gmail.com>
2021-04-08 17:53:10 +0900

    connect H2 Database using JdbcMemberRepository

--- src/main/java/helloalpaca/hellospring/SpringConfig.java
+++ src/main/java/helloalpaca/hellospring/SpringConfig.java
@@ -1,13 +1,20 @@
 package helloalpaca.hellospring;
 
+import helloalpaca.hellospring.repository.JdbcMemberRepository;
 import helloalpaca.hellospring.repository.MemberRepository;
 import helloalpaca.hellospring.repository.MemoryMemberRepository;
 import helloalpaca.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    @Autowired
+    private DataSource datasource;
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +23,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(datasource);
     }
 
 }
